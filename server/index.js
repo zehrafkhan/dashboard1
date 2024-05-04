22978f587f6571770913fc5ae10155bf6aaa643d
unknown <zehrafkhan786@gmail.com>
2024-05-05 00:01:03 +0530

    dashboard setup completed

---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import connectDB from './database/db';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
+
+/*CONFIGURATION*/
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+//Routes
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
+
+ //Mongoose Setup
+ const PORT = process.env.PORT || 9000;
+//connection to database
+connectDB(process.env.MONGO_ATLAS_PW);
+
+//server listenng 
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
